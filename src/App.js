6b15a9aa1bf8a984c926c972d80f81de9fa9df87
RomanEvd <evdokimov.roman.1993@mail.ru>
2023-02-02 21:39:26 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DataExample from "./components/DataExample";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <DataExample endpoint={"data"} title={"Обычная загрузка"} />
+      <DataExample endpoint={"error"} title={"Ошибка"} />
+      <DataExample endpoint={"loading"} title={"Загрузка с задержкой"} />
+    </>
   );
 }
 
